372c480ab9dec11bf1ec0af84a31865f25eb6175
tomlykke <tomlykke@mailinator.com>
2017-09-27 01:56:19 +0500

    tune tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 registerServiceWorker();
