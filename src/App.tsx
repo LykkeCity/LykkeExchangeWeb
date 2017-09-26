fdb75cfbfa1e0d299384c7da6515e365f63fecde
tomlykke <tomlykke@mailinator.com>
2017-09-27 03:08:55 +0500

    add ant design lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import {Layout} from 'antd';
 import * as React from 'react';
 import './App.css';
+
+const {Header, Content, Footer} = Layout
 
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <h2>Lykke Exchange Web</h2>
-      </div>
+      <Layout className="app">
+        <Header className="app__header">
+          Lykke
+        </Header>
+        <Content className="app__shell">
+          Lykke Exchange web app
+        </Content>
+        <Footer>&copy; 2017 Lykke, Inc.</Footer>
+      </Layout>
     );
   }
 }
