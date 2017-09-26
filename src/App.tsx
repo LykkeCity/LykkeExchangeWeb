b71292f25ac28047b64bacff8d47eb4ab61c78b1
tomlykke <tomlykke@mailinator.com>
2017-09-27 01:41:08 +0500

    Bootstrap an app with CRA

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h2>
+          Lykke Exchange Web
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
