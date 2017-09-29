dbb96de2cea91a2726a5bb4de0c3e76e06da753d
tomlykke <tomlykke@mailinator.com>
2017-09-29 15:33:50 +0500

    Bump build

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import {Layout} from 'antd';
 import * as React from 'react';
 import './App.css';
 
-const {Header, Content, Footer} = Layout
+const {Header, Content, Footer} = Layout;
 
 class App extends React.Component {
   render() {
     return (
       <Layout className="app">
-        <Header className="app__header">
-          Lykke
-        </Header>
-        <Content className="app__shell">
-          Lykke Exchange web app
-        </Content>
+        <Header className="app__header">LykkeWallet</Header>
+        <Content className="app__shell">LykkeWallet</Content>
         <Footer>&copy; 2017 Lykke, Inc.</Footer>
       </Layout>
     );
