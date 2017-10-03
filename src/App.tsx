9c6609f23ab079a678c94b7bf8540afaf73de7e3
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-10-03 23:31:42 +0500

    add react-router dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {Layout} from 'antd';
 import * as React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 const {Header, Content, Footer} = Layout;
@@ -7,11 +8,15 @@ const {Header, Content, Footer} = Layout;
 class App extends React.Component {
   render() {
     return (
-      <Layout className="app">
-        <Header className="app__header">LykkeWallet</Header>
-        <Content className="app__shell">LykkeWallet</Content>
-        <Footer>&copy; 2017 Lykke, Inc.</Footer>
-      </Layout>
+      <Router>
+        <div>
+          <Layout className="app">
+            <Header className="app__header">LykkeWallet</Header>
+            <Content className="app__shell">LykkeWallet</Content>
+            <Footer>&copy; 2017 Lykke, Inc.</Footer>
+          </Layout>
+        </div>
+      </Router>
     );
   }
 }
