4a0a814225a23d275aa8ea7acd6562695abc4132
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-10-10 23:24:41 +0500

    Fix broken tests

---
+++ src/utils/testUtils.ts
@@ -0,0 +1,19 @@
+export class LocalStorageMock {
+  store: any = {};
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -1,8 +1,12 @@
+import {LocalStorageMock} from './testUtils';
+
 const TOKEN_KEY = 'lww-token';
 
+const storage = window.localStorage || new LocalStorageMock();
+
 export const set = (value: any) =>
-  localStorage.setItem(TOKEN_KEY, JSON.stringify(value));
+  storage.setItem(TOKEN_KEY, JSON.stringify(value));
 
-export const get = () => JSON.parse(localStorage.getItem(TOKEN_KEY)!);
+export const get = () => JSON.parse(storage.getItem(TOKEN_KEY)!);
 
-export const clear = () => localStorage.removeItem(TOKEN_KEY);
+export const clear = () => storage.removeItem(TOKEN_KEY);
