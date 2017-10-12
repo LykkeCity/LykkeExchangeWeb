ef8ede8ada6483f25ee8f4f97bee1b59aad6f60e
daycoder <daycoder@icloud.com>
2017-10-12 13:13:56 +0300

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import {Layout} from 'antd';
-import Col from 'antd/lib/grid/col';
-import Row from 'antd/lib/grid/row';
 import Menu from 'antd/lib/menu';
 import {Provider} from 'mobx-react';
 import * as React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import Copyright from './components/Copyright';
 import Header from './components/Header';
-import Social from './components/Social';
+import Footer from './components/Footer';
 import {ROUTE_AUTH, ROUTE_LOGIN, ROUTE_WALLET} from './constants/routes';
 import {LoginPage, WalletPage} from './pages';
 import AuthPage from './pages/AuthPage';
 import {RootStore} from './stores';
 
-const {Content, Footer} = Layout;
+const {Content} = Layout;
 const rootStore = new RootStore();
 
 export interface InjectedRootStoreProps {
@@ -39,16 +36,7 @@ class App extends React.Component {
               <Route exact={true} path={ROUTE_LOGIN} component={LoginPage} />
               <Route exact={true} path={ROUTE_AUTH} component={AuthPage} />
             </Content>
-            <Footer>
-              <Row>
-                <Col span={12}>
-                  <Copyright />
-                </Col>
-                <Col span={12} style={{textAlign: 'right'}}>
-                  <Social />
-                </Col>
-              </Row>
-            </Footer>
+            <Footer />
           </Layout>
         </Router>
       </Provider>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,49 @@
 import * as React from 'react';
 
+import Social from '../Social';
+import Apps from '../Apps';
+import Copyright from '../Copyright';
+import Subscribe from '../Blocks/Subscribe';
+import Nav from './nav';
+import './style.css';
+
 export default () => (
-  <div>&copy; 2016 Lykke. All rights reserved</div>
+  <footer className="footer">
+    <div className="footer__top">
+      <div className="container">
+        <div className="separator"></div>
+
+        <div className="row">
+          <div className="col-md-5">
+            <Apps theme="left" />
+            <Subscribe />
+          </div>
+          <div className="col-md-7">
+            <Nav />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="footer__bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-7 pull-right">
+            <Social theme="footer"/>
+          </div>
+          <div className="col-xs-12 col-sm-5">
+            <ul className="footer_links">
+              <li>
+                <Copyright />
+              </li>
+              <li className="middot">&middot;</li>
+              <li><a href="">Privacy Policy</a></li>
+              <li className="middot">&middot;</li>
+              <li><a href="">Terms of Use</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 )
