2469024581b636fa05b6bd9df5e293e3e26e9de3
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-10-26 01:59:06 +0500

    Refactor ProtectedRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import cn from 'classnames';
+import classnames from 'classnames';
 import {inject, observer} from 'mobx-react';
 import * as React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import NoMatch from './components/NoMatch';
+import ProtectedRoute from './components/ProtectedRoute/index';
 import TransferResult from './components/TransferResult';
 import {ROUTE_AUTH, ROUTE_TRANSFER, ROUTE_WALLET} from './constants/routes';
+import {STORE_ROOT} from './constants/stores';
 import {WalletPage} from './pages';
 import AuthPage from './pages/AuthPage';
 import TransferPage from './pages/TransferPage';
 import {RootStore} from './stores';
 
 import './App.css';
-import {STORE_ROOT} from './constants/stores';
 
 export interface RootStoreProps {
   rootStore?: RootStore;
@@ -28,20 +29,24 @@ class App extends React.Component<RootStoreProps> {
     return (
       <Router>
         <div
-          className={cn('app', {
+          className={classnames('app', {
             'app--overlayed': this.props.rootStore!.uiStore.overlayed
           })}
         >
           <Header />
           <div className="app__shell">
             <Switch>
-              <Route exact={true} path={ROUTE_WALLET} component={WalletPage} />
-              <Route
+              <ProtectedRoute
+                exact={true}
+                path={ROUTE_WALLET}
+                component={WalletPage}
+              />
+              <ProtectedRoute
                 exact={true}
                 path={`${ROUTE_WALLET}/:walletId${ROUTE_TRANSFER}`}
                 component={TransferPage}
               />
-              <Route
+              <ProtectedRoute
                 path={`${ROUTE_TRANSFER}/success`}
                 component={TransferResult}
               />
--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,33 +1,22 @@
+import {inject, observer} from 'mobx-react';
 import * as React from 'react';
 import {Redirect, Route, RouteProps} from 'react-router';
+import {RootStoreProps} from '../../App';
 import {ROUTE_LOGIN} from '../../constants/routes';
-import {AuthStore} from '../../stores/index';
+import {STORE_ROOT} from '../../constants/stores';
+// import {AuthStore} from '../../stores/index';
 
-interface ProtectedRouteProps extends RouteProps {
-  Component: React.ComponentClass<any> | React.SFC<any>;
-  authStore: AuthStore;
-}
+type ProtectedRouteProps = RouteProps & RootStoreProps;
 
-export const ProtectedRoute = ({
-  Component,
-  authStore,
-  ...rest
-}: ProtectedRouteProps) => (
-  <Route
-    {...rest}
-    // tslint:disable-next-line:jsx-no-lambda
-    component={(props: any) =>
-      !!authStore.token ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: ROUTE_LOGIN,
-            state: {from: props.location}
-          }}
-        />
-      )}
-  />
-);
+export const ProtectedRoute = ({rootStore, ...rest}: ProtectedRouteProps) =>
+  !!rootStore!.authStore.token ? (
+    <Route {...rest} />
+  ) : (
+    <Redirect
+      to={{
+        pathname: ROUTE_LOGIN
+      }}
+    />
+  );
 
-export default ProtectedRoute;
+export default inject(STORE_ROOT)(observer(ProtectedRoute));
