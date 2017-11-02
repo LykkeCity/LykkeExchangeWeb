3503f8d58102051d5b9bca6494f0d5815f00bb7a
Julia Tolipova <julia.tolipova@gmail.com>
2017-11-02 14:47:48 +0100

    introduced format properties with default value for NumberFormat component

--- src/components/NumberFormat/index.tsx
+++ src/components/NumberFormat/index.tsx
@@ -1,13 +1,12 @@
-import {observer} from 'mobx-react';
 import * as numeral from 'numeral';
 import * as React from 'react';
 
 interface NumberFormatProps {
   value: number;
+  format?: string;
 }
 
-export const NumberFormat: React.SFC<NumberFormatProps> = ({value}) => (
-  <span>{numeral(value).format('0,0[.]00')}</span>
-);
-
-export default observer(NumberFormat);
+export const NumberFormat: React.SFC<NumberFormatProps> = ({
+  value,
+  format = '0,0[.]00'
+}) => <span>{numeral(value).format(format)}</span>;
