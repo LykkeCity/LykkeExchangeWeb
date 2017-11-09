33543118ec5fc72d65c1583d62ed12f53f0c0cde
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-11-09 20:43:14 +0500

    Fix routes for transfer

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -6,9 +6,9 @@ export const ROUTE_WALLETS_HFT = '/wallets/hft';
 
 export const ROUTE_TRANSFER = '/transfer/:dest/:walletId';
 export const ROUTE_TRANSFER_FROM = (walletId: string) =>
-  `transfer/from/${walletId}`;
+  `/transfer/from/${walletId}`;
 export const ROUTE_TRANSFER_TO = (walletId: string) =>
-  `transfer/to/${walletId}`;
+  `/transfer/to/${walletId}`;
 export const ROUTE_TRANSFER_SUCCESS = '/transfer/success';
 
 export const ROUTE_AUTH = '/auth';
