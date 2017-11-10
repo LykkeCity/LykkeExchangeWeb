cd3dfffc16bfd980fe102d25922cc2595d5f84f9
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-11-10 23:45:16 +0500

    Unregister sw

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import {Provider} from 'mobx-react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import {RootStore} from './stores';
 
 const rootStore = new RootStore();
@@ -14,4 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+// registerServiceWorker();
