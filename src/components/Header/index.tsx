1fdf4e2ce9d84fea45840246a8b40a6f3992a188
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-11-17 17:36:38 +0500

    Move store injection to the Navbar component
    Style fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,15 @@
-import {inject, observer} from 'mobx-react';
 import * as React from 'react';
-import {RootStoreProps} from '../../App';
 import SideBar from '../Sidebar';
 import HeaderBar from './headerbar';
 import NavBar from './navbar';
 import './style.css';
 
-interface HeaderProps {
-  isAuthenticated?: boolean;
-}
-
-export const Header: React.SFC<HeaderProps> = ({isAuthenticated}) => (
+export const Header = () => (
   <div>
     <SideBar />
     <HeaderBar />
-    {isAuthenticated && <NavBar />}
+    <NavBar />
   </div>
 );
 
-export default inject(({rootStore}: RootStoreProps) => ({
-  isAuthenticated: rootStore!.authStore.isAuthenticated
-}))(observer(Header));
+export default Header;
