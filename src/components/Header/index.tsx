40f08ea811248bf3202d2b93db8a33024699e359
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-11-17 17:09:47 +0500

    Hide headerbar for unauthorized users

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
+import {inject, observer} from 'mobx-react';
 import * as React from 'react';
+import {RootStoreProps} from '../../App';
 import SideBar from '../Sidebar';
 import HeaderBar from './headerbar';
 import NavBar from './navbar';
 import './style.css';
 
-export default () => (
+interface HeaderProps {
+  isAuthenticated?: boolean;
+}
+
+export const Header: React.SFC<HeaderProps> = ({isAuthenticated}) => (
   <div>
     <SideBar />
     <HeaderBar />
-    <NavBar />
+    {isAuthenticated && <NavBar />}
   </div>
 );
+
+export default inject(({rootStore}: RootStoreProps) => ({
+  isAuthenticated: rootStore!.authStore.isAuthenticated
+}))(observer(Header));
