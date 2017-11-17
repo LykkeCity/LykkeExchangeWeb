5881742117f0a25dfac6bb4038679a92dc6cf3c8
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-11-17 20:53:57 +0500

    Add validation scenarios for signin/signup

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,6 +1,7 @@
 import Button from 'antd/lib/button/button';
 import Form from 'antd/lib/form/Form';
 import Input from 'antd/lib/input/Input';
+import {runInAction} from 'mobx';
 import {inject, observer} from 'mobx-react';
 import * as React from 'react';
 import {Redirect} from 'react-router';
@@ -13,16 +14,36 @@ interface LoginFormProps extends RootStoreProps {
   form: any;
 }
 
+interface LoginFormState extends RootStoreProps {
+  errors: any;
+}
+
 const FormItem = Form.Item;
 
-export class LoginForm extends React.Component<LoginFormProps> {
+export class LoginForm extends React.Component<LoginFormProps, LoginFormState> {
   readonly authStore = this.props.rootStore!.authStore;
+  state: LoginFormState = {
+    errors: ''
+  };
 
   handleSubmit = (e: any) => {
     e.preventDefault();
     this.props.form.validateFields((err: any, values: any) => {
       if (!err) {
-        this.authStore.login(values.email, values.password);
+        this.authStore
+          .login(values.email, values.password)
+          .then(resp => {
+            const {authStore} = this.props.rootStore!;
+            runInAction(() => {
+              authStore.token = resp.AccessToken;
+            });
+          })
+          .catch(error => {
+            this.setState({errors: JSON.parse(error.message)});
+            setTimeout(() => {
+              this.setState({errors: undefined});
+            }, 3000);
+          });
       }
     });
   };
@@ -31,7 +52,22 @@ export class LoginForm extends React.Component<LoginFormProps> {
     e.preventDefault();
     this.props.form.validateFields((err: any, values: any) => {
       if (!err) {
-        this.authStore.signup(values.email, values.password);
+        this.authStore
+          .signup(values.email, values.password)
+          .then(resp => {
+            const {profileStore, authStore} = this.props.rootStore!;
+            runInAction(() => {
+              profileStore.firstName = resp.PersonalData.FirstName;
+              profileStore.lastName = resp.PersonalData.LastName;
+              authStore.token = resp.Token;
+            });
+          })
+          .catch(error => {
+            this.setState({errors: JSON.parse(error.message)});
+            setTimeout(() => {
+              this.setState({errors: undefined});
+            }, 3000);
+          });
       }
     });
   };
@@ -63,6 +99,12 @@ export class LoginForm extends React.Component<LoginFormProps> {
               className="form-control"
             />
           )}
+          {!!this.state.errors &&
+            Object.keys(this.state.errors).map(x => (
+              <div key={x} className="ant-form-explain">
+                {this.state.errors[x]}
+              </div>
+            ))}
         </FormItem>
         <FormItem>
           <Button
--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -29,19 +29,11 @@ export class AuthStore {
     );
   }
 
-  login = async (username: string, password: string) => {
-    const resp = await this.api!.login(username, password);
-    this.token = resp.AccessToken;
-  };
+  login = (username: string, password: string) =>
+    this.api!.login(username, password);
 
-  signup = async (username: string, password: string) => {
-    const resp = await this.api!.signup(username, password);
-    runInAction(() => {
-      this.rootStore.profileStore.firstName = resp.PersonalData.FirstName;
-      this.rootStore.profileStore.lastName = resp.PersonalData.LastName;
-      this.token = resp.Token;
-    });
-  };
+  signup = async (username: string, password: string) =>
+    this.api!.signup(username, password);
 
   auth = async (code: string) => {
     const authContext = await this.fetchBearerToken(code);
