81d34fb2df98524d7f71650093934c39deb49fc6
Sergey Smirnov <sergous@gmail.com>
2017-12-24 15:21:16 +0300

    Lwdev 4764 hot reload with persist root store (#39)
    * Configure webpack-hot-reload
    
    * Configure mobx-persist
    
    * Add production support

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,53 @@
+import {create, persist} from 'mobx-persist';
 import {Provider} from 'mobx-react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import {RootStore} from './stores';
 
-const rootStore = new RootStore();
+export const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <Provider rootStore={rootStore}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+if (process.env.NODE_ENV === 'production') {
+  runProduction();
+} else {
+  runDevelopment();
+}
+
+function runProduction() {
+  const rootStore = new RootStore();
+  renderApp(rootStore);
+}
+
+function runDevelopment() {
+  const rootStore = new RootStore();
+  const persistRootStore = persist(RootStore)(rootStore);
+
+  const hydrate = create({
+    storage: localStorage
+  });
+
+  const result = hydrate('persistRootStore', persistRootStore);
+  const rehydrate = result.rehydrate;
+  // tslint:disable-next-line:no-console
+  result.then(() => console.log('rootStore hydrated'));
+
+  renderApp(persistRootStore);
+
+  if (module.hot) {
+    module.hot.accept(['./App', './stores'], () => {
+      // tslint:disable-next-line:no-console
+      rehydrate().then(() => console.log('rootStore rehydrated'));
+
+      renderApp(persistRootStore);
+    });
+  }
+}
+
+function renderApp(rootStore: any) {
+  ReactDOM.render(
+    <Provider rootStore={rootStore}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+}
