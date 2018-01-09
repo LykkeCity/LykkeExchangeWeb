fffc4586094dc5a0cde548f665bce29a89a4975d
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-01-09 21:44:53 +0500

    Use current year in the copyright

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -1,3 +1,5 @@
 import * as React from 'react';
 
-export default () => <span className="copy">&copy; 2017 Lykke, Inc.</span>;
+export default () => (
+  <span className="copy">&copy; {new Date().getFullYear()} Lykke, Inc.</span>
+);
