5b68bd425b59856ab8373f41571b99d180ea23e3
Sergey Smirnov <sergous@gmail.com>
2018-01-18 20:25:59 +0300

    Style social icon size.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -136,7 +136,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="fb_simple" />
+                  <Icon name="fb_simple" size="20px" />
                   <span> Facebook</span>
                 </a>
               </li>
@@ -146,7 +146,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="tw" />
+                  <Icon name="tw" size="20px" />
                   <span> Twitter</span>
                 </a>
               </li>
@@ -156,7 +156,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="instagram" />
+                  <Icon name="instagram" size="20px" />
                   <span> Instagram</span>
                 </a>
               </li>
@@ -166,7 +166,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="youtube" />
+                  <Icon name="youtube" size="20px" />
                   <span> Youtube</span>
                 </a>
               </li>
@@ -176,7 +176,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="linkedin" />
+                  <Icon name="linkedin" size="20px" />
                   <span> Linkedin</span>
                 </a>
               </li>
@@ -186,7 +186,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="reddit" />
+                  <Icon name="reddit" size="20px" />
                   <span> Reddit</span>
                 </a>
               </li>
@@ -196,7 +196,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="telegram" />
+                  <Icon name="telegram" size="20px" />
                   <span> Telegram</span>
                 </a>
               </li>
@@ -206,7 +206,7 @@ export class Sidebar extends React.Component<RootStoreProps> {
                   target="_blank"
                   className="social__item"
                 >
-                  <Icon name="slack" />
+                  <Icon name="slack" size="20px" />
                   <span> Slack</span>
                 </a>
               </li>
