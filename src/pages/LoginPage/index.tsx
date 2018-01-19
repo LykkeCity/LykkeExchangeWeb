57d0a54a4b72d3ffca6669f41553ccc19b6ae1dd
Sergey Smirnov <sergous@gmail.com>
2018-01-19 14:42:50 +0300

    Revert update LoginPage in "Add menu-overlay to app."
    This reverts commit 98d4eae1d08929165af97cb02c81b10c0592f863.

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,6 +1,4 @@
-import * as classNames from 'classnames';
 import {inject, observer} from 'mobx-react';
-import {computed} from 'mobx/lib/mobx';
 import * as React from 'react';
 import {RootStoreProps} from '../../App';
 import Footer from '../../components/Footer';
@@ -10,20 +8,13 @@ import Logo from '../../components/Logo';
 import {STORE_ROOT} from '../../constants/stores';
 
 export class LoginPage extends React.Component<RootStoreProps> {
-  @computed
-  private get classes() {
-    return {
-      'app--overlayed': this.props.rootStore!.uiStore.overlayed
-    };
-  }
-
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
   render() {
     return (
-      <div className={classNames(this.classes)}>
+      <div>
         <Header />
         <div className="login">
           <Logo />
