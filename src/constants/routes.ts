bf82b3e1931b37a1b0181150837a0aa3317fb444
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-09 17:14:03 +0300

    LWDEV-6220: Added DepositCreditCard routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -17,4 +17,12 @@ export const ROUTE_AFFILIATE = '/affiliate';
 export const ROUTE_AFFILIATE_STATISTICS = `${ROUTE_AFFILIATE}/statistics`;
 export const ROUTE_AFFILIATE_DETAILS = `${ROUTE_AFFILIATE}/details`;
 
+export const ROUTE_DEPOSIT_CREDIT_CARD_BASE = '/deposit/credit-card';
+export const ROUTE_DEPOSIT_CREDIT_CARD = `${ROUTE_DEPOSIT_CREDIT_CARD_BASE}/:walletId/:assetId?`;
+export const ROUTE_DEPOSIT_CREDIT_CARD_TO = (walletId: string, assetId = '') =>
+  `${ROUTE_DEPOSIT_CREDIT_CARD_BASE}/${walletId}/${assetId}`;
+export const ROUTE_DEPOSIT_CREDIT_CARD_GATEWAY = `${ROUTE_DEPOSIT_CREDIT_CARD_BASE}/gateway`;
+export const ROUTE_DEPOSIT_CREDIT_CARD_SUCCESS = `${ROUTE_DEPOSIT_CREDIT_CARD_BASE}/success`;
+export const ROUTE_DEPOSIT_CREDIT_CARD_FAIL = `${ROUTE_DEPOSIT_CREDIT_CARD_BASE}/fail`;
+
 export const ROUTE_AUTH = '/auth';
