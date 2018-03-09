1cea4b92a1c0df0f94d37a14a32e5fbd7f703131
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-09 17:16:53 +0300

    LWDEV-6220: Added Deposit Result components

--- src/pages/WalletPage/index.tsx
+++ src/pages/WalletPage/index.tsx
@@ -20,6 +20,8 @@ export class WalletPage extends React.Component<RootStoreProps> {
   private readonly walletStore = this.props.rootStore!.walletStore;
   private readonly uiStore = this.props.rootStore!.uiStore;
   private readonly transferStore = this.props.rootStore!.transferStore;
+  private readonly depositCreditCardStore = this.props.rootStore!
+    .depositCreditCardStore;
 
   @observable private wallet = new WalletModel(this.walletStore);
   @observable private activeStep = 1;
@@ -27,6 +29,7 @@ export class WalletPage extends React.Component<RootStoreProps> {
   constructor(props: any) {
     super(props);
     this.transferStore.resetCurrentTransfer();
+    this.depositCreditCardStore.resetCurrentDeposit();
   }
 
   render() {
