b8d693d60bc7cef7bc9ca70a091bba87850cc892
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-09 17:16:13 +0300

    LWDEV-6220: Added Deposit Credit Card page with form

--- src/pages/ProtectedPage/index.tsx
+++ src/pages/ProtectedPage/index.tsx
@@ -8,7 +8,10 @@ import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import {loadable} from '../../components/hoc/loadable';
 import {NoMatch} from '../../components/NoMatch/index';
+import PaymentGateway from '../../components/PaymentGateway';
 
+import {DepositFail} from '../../components/DepositResult';
+import DepositSuccess from '../../components/DepositResult';
 import {
   TransferFail,
   TransferResult
@@ -17,6 +20,10 @@ import {
   ROUTE_AFFILIATE,
   ROUTE_AFFILIATE_DETAILS,
   ROUTE_AFFILIATE_STATISTICS,
+  ROUTE_DEPOSIT_CREDIT_CARD,
+  ROUTE_DEPOSIT_CREDIT_CARD_FAIL,
+  ROUTE_DEPOSIT_CREDIT_CARD_GATEWAY,
+  ROUTE_DEPOSIT_CREDIT_CARD_SUCCESS,
   ROUTE_ROOT,
   ROUTE_TRANSFER,
   ROUTE_TRANSFER_BASE,
@@ -26,7 +33,7 @@ import {
   ROUTE_WALLETS_TRADING
 } from '../../constants/routes';
 import {STORE_ROOT} from '../../constants/stores';
-import {WalletPage} from '../../pages/index';
+import {DepositCreditCardPage, WalletPage} from '../../pages/index';
 import AffiliatePage from '../AffiliatePage/index';
 import TransferPage from '../TransferPage/index';
 
@@ -37,6 +44,7 @@ export class ProtectedPage extends React.Component<RootStoreProps> {
   private readonly assetStore = this.props.rootStore!.assetStore;
   private readonly affiliateStore = this.props.rootStore!.affiliateStore;
   private readonly featureStore = this.props.rootStore!.featureStore;
+  private readonly appSettingsStore = this.props.rootStore!.appSettingsStore;
 
   @computed
   private get classes() {
@@ -51,6 +59,8 @@ export class ProtectedPage extends React.Component<RootStoreProps> {
     this.featureStore.getFeatures();
     this.assetStore
       .fetchCategories()
+      .then(() => this.appSettingsStore.fetchSettings())
+      .then(() => this.appSettingsStore.fetchCountryCodes())
       .then(() => this.assetStore.fetchAssets())
       .then(() => this.profileStore.fetchUserInfo())
       .then(() => this.walletStore.fetchWallets())
@@ -99,6 +109,22 @@ export class ProtectedPage extends React.Component<RootStoreProps> {
             <Route
               path={ROUTE_AFFILIATE}
               component={asLoading(AffiliatePage)}
+            />
+            <Route
+              path={ROUTE_DEPOSIT_CREDIT_CARD_GATEWAY}
+              component={PaymentGateway}
+            />
+            <Route
+              path={ROUTE_DEPOSIT_CREDIT_CARD_SUCCESS}
+              component={DepositSuccess}
+            />
+            <Route
+              path={ROUTE_DEPOSIT_CREDIT_CARD_FAIL}
+              component={DepositFail}
+            />
+            <Route
+              path={ROUTE_DEPOSIT_CREDIT_CARD}
+              component={asLoading(DepositCreditCardPage)}
             />
             <Route component={NoMatch} />
           </Switch>
