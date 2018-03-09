2e68ff8cf212b90f577adb6bb50fe8b63dbcc688
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-09 17:04:54 +0300

    LWDEV-6220: Added dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+export {default as Dropdown} from './dropdown';
+export {default as DropdownContainer} from './dropdownContainer';
+export {default as DropdownControl} from './dropdownControl';
+export {default as DropdownList} from './dropdownList';
+export {default as DropdownListItem} from './dropdownListItem';
