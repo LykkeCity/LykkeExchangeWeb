079637f2d9b28128f6055e7d630104fc35e49ae2
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-16 11:01:23 +0300

    LWDEV-6220: Added assets submenu to wallet deposit dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,6 +1,6 @@
 import './style.css';
 
-export {default as Dropdown} from './dropdown';
+export {default as Dropdown, DropdownPosition} from './dropdown';
 export {default as DropdownContainer} from './dropdownContainer';
 export {default as DropdownControl} from './dropdownControl';
 export {default as DropdownList} from './dropdownList';
