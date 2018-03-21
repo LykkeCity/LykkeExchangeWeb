8a6472ee6f0f3e43c7069d0205e52ad6ea210bec
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-21 11:47:37 +0300

    LWDEV-5081: Fetch wallets on route change

--- src/pages/ProtectedPage/index.tsx
+++ src/pages/ProtectedPage/index.tsx
@@ -2,7 +2,7 @@ import * as classNames from 'classnames';
 import {computed} from 'mobx';
 import {inject, observer} from 'mobx-react';
 import * as React from 'react';
-import {Redirect, Route, Switch} from 'react-router-dom';
+import {Redirect, Route, RouteComponentProps, Switch} from 'react-router-dom';
 import {RootStoreProps} from '../../App';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
@@ -30,13 +30,16 @@ import {WalletPage} from '../../pages/index';
 import AffiliatePage from '../AffiliatePage/index';
 import TransferPage from '../TransferPage/index';
 
-export class ProtectedPage extends React.Component<RootStoreProps> {
+export class ProtectedPage extends React.Component<
+  RootStoreProps & RouteComponentProps<any>
+> {
   private readonly walletStore = this.props.rootStore!.walletStore;
   private readonly profileStore = this.props.rootStore!.profileStore;
   private readonly uiStore = this.props.rootStore!.uiStore;
   private readonly assetStore = this.props.rootStore!.assetStore;
   private readonly affiliateStore = this.props.rootStore!.affiliateStore;
   private readonly featureStore = this.props.rootStore!.featureStore;
+  private unlistenRouteChange: () => void;
 
   @computed
   private get classes() {
@@ -56,6 +59,15 @@ export class ProtectedPage extends React.Component<RootStoreProps> {
       .then(() => this.walletStore.fetchWallets())
       .then(() => this.profileStore.fetchBaseAsset())
       .then(() => this.uiStore.finishRequest());
+
+    this.unlistenRouteChange = this.props.history.listen(() => {
+      this.uiStore.startRequest();
+      this.walletStore.fetchWallets().then(() => this.uiStore.finishRequest());
+    });
+  }
+
+  componentWillUnmount() {
+    this.unlistenRouteChange();
   }
 
   render() {
