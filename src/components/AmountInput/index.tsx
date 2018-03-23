691a0eff81cff9f89679dfba2152764de6c46349
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-23 09:11:06 +0300

    LWDEV-6220: Rewrite AmountInput as component

--- src/components/AmountInput/index.tsx
+++ src/components/AmountInput/index.tsx
@@ -4,13 +4,22 @@ import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 // tslint:disable-next-line:no-var-requires
 const TextMask = require('react-text-mask').default;
 
-export const AmountInput = (
-  onChange: (e: React.ChangeEvent<any>) => void,
-  value: any,
-  name: string,
-  decimalLimit: number = 8,
-  maxLength: number = 10
-) => {
+export interface AmountInputProps {
+  onChange?: (e: React.ChangeEvent<any>) => void;
+  value?: any;
+  name?: string;
+  decimalLimit?: number;
+  maxLength?: number;
+}
+
+export const AmountInput: React.SFC<AmountInputProps> = ({
+  onChange,
+  value,
+  name,
+  decimalLimit = 8,
+  maxLength = 10,
+  ...attributes
+}) => {
   const numberMask = createNumberMask({
     allowDecimal: true,
     allowLeadingZeroes: true,
@@ -28,6 +37,7 @@ export const AmountInput = (
       onChange={onChange}
       value={value || ''}
       maxLength={maxLength}
+      {...attributes}
     />
   );
 };
