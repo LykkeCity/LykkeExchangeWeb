c06f7e421b4cdf6f9f873b88b71ff281fe24b82a
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-04-26 10:19:54 +0300

    LWW-74: Introduced Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,34 @@
+import classnames from 'classnames';
+import * as React from 'react';
+import './style.css';
+
+interface BannerProps {
+  className?: string;
+  text?: string;
+  title?: string;
+  footer?: string | React.ReactChild;
+  show?: boolean;
+}
+
+export const Banner: React.SFC<BannerProps> = ({
+  className,
+  text,
+  title,
+  footer,
+  show = true,
+  ...attributes
+}) => {
+  return (
+    <div
+      {...attributes}
+      className={classnames('banner', className, {banner_hidden: !show})}
+    >
+      <div className="banner__bar" />
+      {title && <div className="banner__title">{title}</div>}
+      {text && <div className="banner__text">{text}</div>}
+      {footer && <div className="banner__footer">{footer}</div>}
+    </div>
+  );
+};
+
+export default Banner;
