16d5c13e3d3e74b2a48bd5406e88237083722e69
Olga Yaroshchuk <o.yaroshchuk@itransition.com>
2018-05-03 12:48:38 +0300

    LWW-73 Migrate settings and credit card functionality

--- src/api/appSettingsApi.ts
+++ src/api/appSettingsApi.ts
@@ -8,13 +8,11 @@ export interface AppSettingsApi {
 
 export class RestAppSettingsApi extends RestApiv1 implements AppSettingsApi {
   fetchSettings = () =>
-    this.get('/AppSettings').catch(
+    this.get('/AppSetting').catch(
       this.rootStore.authStore.redirectToAuthServer
     );
   fetchCountryCodes = () =>
-    this.get('/CountryPhoneCodes').catch(
-      this.rootStore.authStore.redirectToAuthServer
-    );
+    this.get('/Catalogs').catch(this.rootStore.authStore.redirectToAuthServer);
 }
 
 export default RestAppSettingsApi;
