149991c60ffee568298dc830e581ee64eee10aaf
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-06-20 13:19:39 +0300

    LWW-186: Reflected BE changes

--- src/components/DepositCreditCardForm/index.tsx
+++ src/components/DepositCreditCardForm/index.tsx
@@ -50,7 +50,7 @@ export const DepositCreditCardForm: React.SFC<DepositCreditCardFormProps> = ({
     `Field ${fieldName} should not be empty`;
   const DISCLAIMER_ERROR_MESSAGE = 'User has pending disclaimer';
   const DAILY_LIMIT_ERROR_MESSAGE =
-    'Operation is not allowed because of Daily limitations';
+    'Operation is not allowed because of the Limitations';
 
   return (
     <Formik
