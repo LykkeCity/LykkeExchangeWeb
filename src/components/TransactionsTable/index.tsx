60a73f73cc8e9f82b37b2bf510d899b776e444f5
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-08-17 09:24:30 +0300

    LWW-171: Cleanup

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -98,25 +98,6 @@ export class TransactionsTable extends React.Component<TransactionsTableProps> {
   }
 
   render() {
-    const transactionFilters = [
-      {
-        label: 'All',
-        value: []
-      },
-      {
-        label: 'Deposit & Withdraw',
-        value: [TransactionType.CashIn, TransactionType.CashOut]
-      },
-      {
-        label: 'Trading',
-        value: [
-          TransactionType.Trade,
-          TransactionType.LimitTrade,
-          TransactionType.LimitOrderEvent
-        ]
-      }
-    ];
-
     return (
       <div className="transactions">
         <div
@@ -139,42 +120,7 @@ export class TransactionsTable extends React.Component<TransactionsTableProps> {
                 >
                   {this.props.stickyTitle}
                 </div>
-                <div>
-                  {transactionFilters.map(filter => (
-                    <span
-                      className={classnames('transaction-filters__item', {
-                        'transaction-filters__item_active': arraysEqual(
-                          this.transactionsFilterValue,
-                          filter.value
-                        )
-                      })}
-                      key={filter.label}
-                      // tslint:disable-next-line:jsx-no-lambda
-                      onClick={() =>
-                        this.handleTransactionsFilterChange(filter.value, true)}
-                    >
-                      {filter.label}
-                    </span>
-                  ))}
-                  <span
-                    className={classnames('btn-shadow btn-export', {
-                      'has-spinner': this.props.isExportLoading
-                    })}
-                    onClick={this.handleExportClick}
-                  >
-                    {this.props.isExportLoading ? (
-                      <Spinner />
-                    ) : (
-                      <span>
-                        <img
-                          src={`${process.env
-                            .PUBLIC_URL}/images/export-icn.svg`}
-                        />
-                        CSV
-                      </span>
-                    )}
-                  </span>
-                </div>
+                <div>{this.renderFiltersRow(true)}</div>
               </div>
             </div>
           </div>
@@ -202,44 +148,7 @@ export class TransactionsTable extends React.Component<TransactionsTableProps> {
               <div className="transaction-filters__title">
                 Latest transactions
               </div>
-              <div>
-                {transactionFilters.map(filter => (
-                  <span
-                    className={classnames('transaction-filters__item', {
-                      'transaction-filters__item_active': arraysEqual(
-                        this.transactionsFilterValue,
-                        filter.value
-                      )
-                    })}
-                    key={filter.label}
-                    // tslint:disable-next-line:jsx-no-lambda
-                    onClick={() =>
-                      this.handleTransactionsFilterChange(filter.value)}
-                  >
-                    {filter.label}
-                  </span>
-                ))}
-                {this.props.showExportButton && (
-                  <span
-                    className={classnames('btn-shadow btn-export', {
-                      'has-spinner': this.props.isExportLoading
-                    })}
-                    onClick={this.handleExportClick}
-                  >
-                    {this.props.isExportLoading ? (
-                      <Spinner />
-                    ) : (
-                      <span>
-                        <img
-                          src={`${process.env
-                            .PUBLIC_URL}/images/export-icn.svg`}
-                        />
-                        CSV
-                      </span>
-                    )}
-                  </span>
-                )}
-              </div>
+              <div>{this.renderFiltersRow()}</div>
             </div>
           </div>
         </div>
@@ -344,6 +253,65 @@ export class TransactionsTable extends React.Component<TransactionsTableProps> {
       </div>
     );
   }
+
+  private renderFiltersRow = (isSticky?: boolean) => {
+    const transactionFilters = [
+      {
+        label: 'All',
+        value: []
+      },
+      {
+        label: 'Deposit & Withdraw',
+        value: [TransactionType.CashIn, TransactionType.CashOut]
+      },
+      {
+        label: 'Trading',
+        value: [
+          TransactionType.Trade,
+          TransactionType.LimitTrade,
+          TransactionType.LimitOrderEvent
+        ]
+      }
+    ];
+
+    return (
+      <div>
+        {transactionFilters.map(filter => (
+          <span
+            className={classnames('transaction-filters__item', {
+              'transaction-filters__item_active': arraysEqual(
+                this.transactionsFilterValue,
+                filter.value
+              )
+            })}
+            key={filter.label}
+            // tslint:disable-next-line:jsx-no-lambda
+            onClick={() =>
+              this.handleTransactionsFilterChange(filter.value, isSticky)}
+          >
+            {filter.label}
+          </span>
+        ))}
+        {(this.props.showExportButton || isSticky) && (
+          <span
+            className={classnames('btn-shadow btn-export', {
+              'has-spinner': this.props.isExportLoading
+            })}
+            onClick={this.handleExportClick}
+          >
+            {this.props.isExportLoading ? (
+              <Spinner />
+            ) : (
+              <span>
+                <img src={`${process.env.PUBLIC_URL}/images/export-icn.svg`} />
+                CSV
+              </span>
+            )}
+          </span>
+        )}
+      </div>
+    );
+  };
 
   private handleExportClick = async () => {
     this.props.exportTransactions(this.transactionsFilterValue);
