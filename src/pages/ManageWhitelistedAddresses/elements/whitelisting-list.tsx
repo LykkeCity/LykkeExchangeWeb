1faa6e82bc3a59e62bb84e61417b527cd592558d
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-11-10 11:19:29 +0300

    LM-1521: added time to the whitelist item

--- src/pages/ManageWhitelistedAddresses/elements/whitelisting-list.tsx
+++ src/pages/ManageWhitelistedAddresses/elements/whitelisting-list.tsx
@@ -102,7 +102,11 @@ export class WhitelistingList extends React.Component<Props> {
                 <FormattedDate
                   day="2-digit"
                   month="2-digit"
-                  year="2-digit"
+                  year="numeric"
+                  hour="2-digit"
+                  minute="2-digit"
+                  second="2-digit"
+                  hour12={false}
                   value={x.createdAt}
                 />
               </td>
